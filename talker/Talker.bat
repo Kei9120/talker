058a7c8d8e17889055701c11f3e2d9a26ad263a2
Kei9120 <71600382+Kei9120@users.noreply.github.com>
2021-02-19 19:38:55 +0900

    Add files via upload

---
+++ talker/Talker.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\Users\KEI\Documents\discordbot\talker
+node server.js
---
+++ talker/tokens.js
@@ -0,0 +1,4 @@
+module.exports = {
+    discord:'NzcwMjIxNzc4NDg3ODY5NDgy.X5aa7g.6pXHSzjxNeOt53qkUB9sNdLpDY0',
+    voiceText:'3ltnd3lx8vdq3g88'
+}
